6f78d1ab9f6145f96aa8ca09df73643feca670cd
Hector Lopez <hectorvmlopez@gmail.com>
2020-11-24 14:42:32 -0500

    using react transition Group

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,10 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
 
 import "./App.css";
 import Modal from "./components/Modal/Modal";
 import Backdrop from "./components/Backdrop/Backdrop";
 import List from "./components/List/List";
-
+import { Transition } from 'react-transition-group';
 
 
 interface Props {
@@ -17,6 +17,7 @@ interface State {
  class App extends Component<Props, State> {
   state = {
     modalIsOpen: false,
+    showBlock: false
   }
   
   showModal = () =>{
@@ -29,20 +30,35 @@ interface State {
 
 
   render() {
+    const duration = 300;//change beetween transition
+    //in Determines if component should be visible
     return (
       <div className="App">
       <h1>React Animations</h1>
       
-        <Modal 
-          show={this.state.modalIsOpen}
-          closed={this.closeModal}
-        />
-        <Backdrop 
-          show={this.state.modalIsOpen}
+      <button className="Button" onClick={() => {
+        this.setState((prevState:any) => {
+          return {showBlock: !prevState.showBlock}
+        });
+      }}>Toogle</button>
 
-        />
-       
-  
+      <Transition in={this.state.showBlock} timeout={duration}>
+        {state => (
+
+        <div style={{
+          backgroundColor: "red",
+          width: 100,
+          height: 100,
+          margin: 'auto',
+          opacity: state === 'exited' ? 0 : 1
+        }}></div> 
+        )}
+
+      </Transition>
+
+      
+      {this.state.modalIsOpen ?<Modal  show={this.state.modalIsOpen} closed={this.closeModal}/> :null}
+      {this.state.modalIsOpen? <Backdrop show={this.state.modalIsOpen}/> : null}
       <button className="Button" onClick={this.showModal}>Open Modal</button>
       <h3>Animating Lists</h3>
       <List />
